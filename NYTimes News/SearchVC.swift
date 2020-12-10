4cfa030d1150374fd4a9d089dd817513f8e65aae
Nada Yehia Dawoud <nadawoud@gmail.com>
2020-12-10 22:31:32 +0200

    Create SearchVC

---
+++ NYTimes News/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  NYTimes News
+//
+//  Created by Nada Yehia Dawoud on 10/12/2020.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}

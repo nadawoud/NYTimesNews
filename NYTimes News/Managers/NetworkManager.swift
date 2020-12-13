d70bc39b2e3e084ace54730cfd9dc7e5dda95550
Nada Yehia Dawoud <nadawoud@gmail.com>
2020-12-13 20:01:03 +0200

    Create NetwotkManager

---
+++ NYTimes News/Managers/NetworkManager.swift
@@ -0,0 +1,60 @@
+//
+//  NetworkManager.swift
+//  NYTimes News
+//
+//  Created by Nada Yehia Dawoud on 11/12/2020.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    private let baseURL = "https://api.nytimes.com/svc/"
+    private let apiKey = "iIzsaMfgygBpoDQAwtr02QXIAdZsNQFt"
+    
+    private init() {}
+    
+    func getMostViewedArticles(in period: Period, completed: @escaping (Result<PopularArticlesData, NYTimesError>) -> Void) {
+        let endpoint = baseURL + "mostpopular/v2/viewed/1.json?api-key=\(apiKey)"
+        
+        guard let url = URL(string: endpoint) else {
+            completed(.failure(.incorrectURL))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let _ = error {
+                completed(.failure(.noInternetConnection))
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidServerResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completed(.failure(.noData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let articlesData = try decoder.decode(PopularArticlesData.self, from: data)
+                completed(.success(articlesData))
+            } catch {
+                print("JSON decoding failed: \(error)")
+                completed(.failure(.invalidData))
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+enum Period: Int {
+    case day = 1
+    case week = 7
+    case month = 30
+}

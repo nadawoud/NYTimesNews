09af78143529d96d8c6d7ee48044d10983e67018
Nada Yehia Dawoud <nadawoud@gmail.com>
2020-12-13 19:56:41 +0200

    Create the model

---
+++ NYTimes News/Model/Article.swift
@@ -0,0 +1,33 @@
+//
+//  Article.swift
+//  NYTimes News
+//
+//  Created by Nada Yehia Dawoud on 11/12/2020.
+//
+
+import Foundation
+
+struct PopularArticlesData: Codable {
+    let numResults: Int
+    let results: [Article]
+}
+
+
+struct Article: Codable {
+    let url: String
+    let publishedDate: String
+    let byline: String
+    let title: String
+    let abstract: String
+    let media:[Media]
+    
+    struct Media: Codable {
+        let caption: String
+        let mediaMetadata: [MediaMetadata]?
+        
+        struct MediaMetadata: Codable {
+            let url: String
+            let format: String
+        }
+    }
+}

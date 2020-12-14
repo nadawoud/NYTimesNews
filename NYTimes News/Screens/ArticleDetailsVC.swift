bec9ec28a3124349a0dc8958fd3ce89dc876f865
Nada Yehia Dawoud <nadawoud@gmail.com>
2020-12-14 23:30:15 +0200

    Setup ArticleDetialsVC to present Article's data

--- NYTimes News/Screens/ArticleDetailsVC.swift
+++ NYTimes News/Screens/ArticleDetailsVC.swift
@@ -9,7 +9,94 @@ import UIKit
 
 class ArticleDetailsVC: UIViewController {
 
+    struct Padding {
+        static let leading: CGFloat = 20
+        static let trailing: CGFloat = -15
+        static let inBetween: CGFloat = 10
+    }
+    
+    var article: Article!
+    
+    let dateLabel = NYTLabel(textAlignment: .left, textStyle: .body, color: .secondaryLabel)
+    let articleTitleLabel = NYTLabel(textAlignment: .left, textStyle: .title1, color: .label)
+    let abstractLabel = NYTLabel(textAlignment: .left, textStyle: .body, color: .label)
+    let byLineLabel = NYTLabel(textAlignment: .left, textStyle: .title3, color: .label)
+    let articleImageView = UIImageView()
+    let captionLabel = NYTLabel(textAlignment: .center, textStyle: .caption1, color: .secondaryLabel)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureDateLabel()
+        configureArticleTitleLabel()
+        configureAbstractLabel()
+        configureByLineLabel()
+        configureArticleImageView()
+    }
+    
+    
+    func configureDateLabel() {
+        view.addSubview(dateLabel)
+        dateLabel.text = article.publishedDate
+        
+        NSLayoutConstraint.activate([
+            dateLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Padding.inBetween),
+            dateLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Padding.leading),
+            dateLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: Padding.trailing),
+            dateLabel.heightAnchor.constraint(equalToConstant: 15)
+        ])
+    }
+    
+    
+    func configureArticleTitleLabel() {
+        view.addSubview(articleTitleLabel)
+        articleTitleLabel.text = article.title
+        
+        NSLayoutConstraint.activate([
+            articleTitleLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: Padding.inBetween),
+            articleTitleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Padding.leading),
+            articleTitleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: Padding.trailing),
+            articleTitleLabel.heightAnchor.constraint(equalToConstant: 140)
+        ])
+    }
+    
+    
+    func configureAbstractLabel() {
+        view.addSubview(abstractLabel)
+        abstractLabel.text = article.abstract.isEmpty ? article.title : article.abstract
+        
+        NSLayoutConstraint.activate([
+            abstractLabel.topAnchor.constraint(equalTo: articleTitleLabel.bottomAnchor, constant: Padding.inBetween),
+            abstractLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Padding.leading),
+            abstractLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: Padding.trailing),
+            abstractLabel.heightAnchor.constraint(equalToConstant: 120)
+        ])
+    }
+    
+    
+    func configureByLineLabel() {
+        view.addSubview(byLineLabel)
+        byLineLabel.text = article.byline
+        
+        NSLayoutConstraint.activate([
+            byLineLabel.topAnchor.constraint(equalTo: abstractLabel.bottomAnchor, constant: Padding.inBetween),
+            byLineLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Padding.leading),
+            byLineLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: Padding.trailing),
+            byLineLabel.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    
+    func configureArticleImageView () {
+        view.addSubview(articleImageView)
+        articleImageView.translatesAutoresizingMaskIntoConstraints = false
+        articleImageView.image = UIImage(named: "placeholder")!
+        
+        NSLayoutConstraint.activate([
+            articleImageView.topAnchor.constraint(equalTo: byLineLabel.bottomAnchor, constant: Padding.inBetween),
+            articleImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            articleImageView.heightAnchor.constraint(equalToConstant: 200),
+            articleImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
     }
 }

84c437d4087cec2dbcc0706f3f3e80b22444f38b
Nada Yehia Dawoud <nadawoud@gmail.com>
2020-12-14 16:09:22 +0200

    Created an extension of UIView

---
+++ NYTimes News/Extension/UIView+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+Ext.swift
+//  NYTimes News
+//
+//  Created by Nada Yehia Dawoud on 13/12/2020.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func pin(to superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: superView.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: superView.trailingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+    }
+}

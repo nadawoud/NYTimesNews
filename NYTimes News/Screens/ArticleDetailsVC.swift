5d636e24edacb5c39efd0f7ddee3992334d7ab45
Nada Yehia Dawoud <nadawoud@gmail.com>
2020-12-14 21:02:39 +0200

    Create ArticleDetailsVC

---
+++ NYTimes News/Screens/ArticleDetailsVC.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleDetailsVC.swift
+//  NYTimes News
+//
+//  Created by Nada Yehia Dawoud on 14/12/2020.
+//
+
+import UIKit
+
+class ArticleDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

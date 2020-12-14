e7ddb00631f989466724d81a90de58221fb023d5
Nada Yehia Dawoud <nadawoud@gmail.com>
2020-12-14 16:17:28 +0200

    Create UITableView and populate with Articles titles and placeholder images

--- NYTimes News/Screens/MostPopularArticlesVC.swift
+++ NYTimes News/Screens/MostPopularArticlesVC.swift
@@ -8,27 +8,59 @@
 import UIKit
 
 class MostPopularArticlesVC: UIViewController {
+    
+    var tableView = UITableView()
+    var articles: [Article] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        getPopularArticles()
+        configureTableView()
+    }
+    
     func configureViewController() {
         view.backgroundColor = .systemBackground
     }
-        
+    
+    
+    func configureTableView() {
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 80
+        tableView.pin(to: view)
+        tableView.register(ArticleCell.self, forCellReuseIdentifier: ArticleCell.reuseID)
     }
     
+    
     func getPopularArticles() {
-        NetworkManager.shared.getMostViewedArticles(in: .week) { (result) in
+        NetworkManager.shared.getMostViewedArticles(in: .week) { [weak self] result in
+            guard let self = self else { return }
+            
             switch result {
             
             case .success(let articlesData):
-                print("PopularArticlesData")
-                print(articlesData)
-                
+                self.articles.append(contentsOf: articlesData.results)
+                DispatchQueue.main.async { self.tableView.reloadData() }
             case .failure(let error):
                 print(error.rawValue)
             }
         }
+    }
+}
+
+
+extension MostPopularArticlesVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ArticleCell.reuseID) as! ArticleCell
+        let article = articles[indexPath.row]
+        cell.set(article: article)
+        
+        return cell
     }
 }

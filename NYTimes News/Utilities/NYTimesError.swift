383b5356b712de6e938c839da8f817f4e1e3084a
Nada Yehia Dawoud <nadawoud@gmail.com>
2021-01-10 22:12:01 +0200

    Update the error messages text

--- NYTimes News/Utilities/NYTimesError.swift
+++ NYTimes News/Utilities/NYTimesError.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 enum NYTimesError: String, Error {
-    case incorrectURL = "The endpoint URL is incorrect!"
-    case noInternetConnection = "Unable to complete your request. Please check your internet connection."
-    case invalidServerResponse = "Invalid Server Response!"
-    case noData = "No data received from sever!"
-    case invalidData = "Data received from sever is invalid!"
+    case incorrectURL = "Unable to complete your request. Please try again."
+    case noInternetConnection = "Couldn't complete your request. Please check your internet connection."
+    case invalidServerResponse = "Uh oh 😅! Something went wrong. Please try again."
+    case noData = "We're very sorry 😅! Something went wrong. Please try again."
+    case invalidData = "This is embarrassing 😅! Would you try again, please."
 }
 

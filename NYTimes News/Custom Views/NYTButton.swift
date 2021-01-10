e7fbaf73b184e1f65685130ddc9a3e7f25eaf594
Nada Yehia Dawoud <nadawoud@gmail.com>
2021-01-10 22:09:30 +0200

    Create NYTButton.swift

---
+++ NYTimes News/Custom Views/NYTButton.swift
@@ -0,0 +1,34 @@
+//
+//  NYTButton.swift
+//  NYTimes News
+//
+//  Created by Nada Yehia Dawoud on 10/01/2021.
+//
+
+import UIKit
+
+class NYTButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
